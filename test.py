a6f6b90f16d4d8768f507140dda69196df4642a7
Himanshu Kumar <hk062848@gmail.com>
2025-09-15 03:42:43 +0530

    removed conn str

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
+from dotenv import load_dotenv
 
-uri = "mongodb+srv://hima:passuser@cluster0.vx6waqn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+load_dotenv()
+uri =  os.getenv("MONGO_URI")
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
